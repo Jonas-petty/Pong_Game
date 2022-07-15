9181aeb657b648e5ece04bce9d9a37b95b62b316
Jonas Felix de Souza <jonaspetty@hotmail.com>
2022-07-14 22:39:48 -0300

    adding sound effects

--- pong.py
+++ pong.py
@@ -12,19 +12,23 @@ def ball_animation():
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
+        pygame.mixer.Sound.play(pong_sound)
         ball_speed_y *= -1
 
     # Player Score
     if ball.left <= 0:
+        pygame.mixer.Sound.play(score_sound)
         player_score += 1
         score_time = pygame.time.get_ticks()
 
     # Opponent Score
     if ball.right >= screen_width:
+        pygame.mixer.Sound.play(score_sound)
         opponent_score += 1
         score_time = pygame.time.get_ticks()
 
     if ball.colliderect(player) and ball_speed_x > 0:
+        pygame.mixer.Sound.play(pong_sound)
         if abs(ball.right - player.left) < 10:
             ball_speed_x *= -1
         elif abs(ball.bottom - player.top) < 10 and ball_speed_y > 0:
@@ -33,6 +37,7 @@ def ball_animation():
             ball_speed_y *= -1
 
     if ball.colliderect(opponent) and ball_speed_x < 0:
+        pygame.mixer.Sound.play(pong_sound)
         if abs(ball.left - opponent.right) < 10:
             ball_speed_x *= -1
         elif abs(ball.bottom - opponent.top) < 10 and ball_speed_y > 0:
@@ -86,6 +91,7 @@ def ball_restart():
 
 
 # General Setup
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -109,14 +115,17 @@ ball_speed_x = 7 * choice((1, -1))
 ball_speed_y = 7 * choice((1, -1))
 player_speed = 0
 opponent_speed = 7
+score_time = True
 
-# Text Variables
+# Score Text
 player_score = 0
 opponent_score = 0
 game_font = pygame.font.Font("freesansbold.ttf", 32)
 
-# Score Timer
-score_time = True
+# Sound
+pong_sound = pygame.mixer.Sound("pong.ogg")
+score_sound = pygame.mixer.Sound("score.ogg")
+
 
 while True:
     for event in pygame.event.get():
